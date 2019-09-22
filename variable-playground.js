c25647d208f8f91fd9524ee52e146632593e9f92
LShetty <lavanya.shetty@b2x.com>
2019-09-22 20:41:29 +0530

    Added variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,24 @@
+let grade = [15, 37]
+
+let addGrade = (val) => {
+  grade.push(57)
+  // debugger
+}
+
+addGrade(grade)
+console.log(grade)
+
+/* Adding debugger in program
+
+Write debugger where needed
+
+----------------------------------*CONSOLE (TERMINAL) COMMANDS:*------------------------
+
+> node debug file_name
+
+Once this is executed, The debugger always pauses in the beginning to skip to the next break in your code which in this case is the debugger which is added in the code
+
+Then type *cont* i.e. continue to continue the program
+By typing *repl* we can inspect variable  and functions  inside the code
+To exit type *kill* then *quit*
+*/
